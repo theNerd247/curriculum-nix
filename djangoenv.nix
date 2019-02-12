34079f26921c4f37497d5c79ebbfc7287da4355e
noah harvey <noah.harvey@generalassemb.ly>
2019-02-12 16:30:34 -0500

    adds djangorestframework to python dependencies

--- djangoenv.nix
+++ djangoenv.nix
@@ -1,7 +1,14 @@
 {stdenv, python36, postgresql}:
 
 let 
-  py = python36.withPackages (ps: with ps; [django_2_1 psycopg2]);
+  py = python36.withPackages (ps: 
+      with ps; 
+      let 
+        dj = django_2_1;
+        rest = djangorestframework.override {django = dj; };
+      in
+        [dj psycopg2 rest]
+    );
 in
 
 stdenv.mkDerivation {
