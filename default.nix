c3d1e6ca4bc0aae7a437ad4459bd435e545afdae
theNerd247 <noah.harvey247 gm>
2019-09-22 15:23:13 -0400

    splits shell.nix into default.nix

--- default.nix
+++ default.nix
@@ -1,10 +1,23 @@
-{stdenv, djangoenv, startdb, nodejsenv}:
+pkgs:
 
-stdenv.mkDerivation {
-  name = "gaenv";
-  buildInputs = [
-    startdb 
-    djangoenv.buildInputs
-    nodejsenv.buildInputs
-  ];
+let
+  makeLessonShell = name: drv: pkgs.mkShell 
+    { buildInputs = [ drv ] ++ drv.buildInputs;
+      shellHook = ''
+        mkdir -p ${name}
+        tar -C ${name} -zxf ${drv}/*.tar.gz 
+      '';
+    };
+in
+{
+  generalAssembly = rec {
+    packages = pkgs.callPackage ./packages {};
+    environments = pkgs.callPackage ./environments { inherit packages; };
+    lessons = pkgs.callPackage ./lessons { inherit environments; };
+    shell = pkgs.lib.attrsets.mapAttrs makeLessonShell lessons 
+      // { schedule = pkgs.mkShell 
+        { buildInputs = [ packages.schedule ];
+        }; 
+      };
+  };
 }
--- shell.nix
+++ shell.nix
@@ -1,24 +1,7 @@
 let 
   config =
   {
-    packageOverrides = pkgs:
-      let
-        makeLessonShell = name: drv: pkgs.mkShell 
-          { buildInputs = [ drv ] ++ drv.buildInputs;
-            shellHook = ''
-              mkdir -p ${name}
-              tar -C ${name} -zxf ${drv}/*.tar.gz 
-            '';
-          };
-      in
-      {
-        generalAssembly = rec {
-          packages = pkgs.callPackage ./packages {};
-          environments = pkgs.callPackage ./environments { inherit packages; };
-          lessons = pkgs.callPackage ./lessons { inherit environments; };
-          shell = pkgs.lib.attrsets.mapAttrs makeLessonShell lessons;
-        };
-      };
+    packageOverrides = import ./default.nix; 
   };
 in
 
